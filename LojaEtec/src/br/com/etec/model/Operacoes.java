f15f6f05cf8b20543526284477bee3bcdd43444c
lab04 <lab04@lab4-10>
2024-08-19 20:55:40 -0300

    adicionado logica de componentes

--- LojaEtec/src/br/com/etec/model/Operacoes.java
+++ LojaEtec/src/br/com/etec/model/Operacoes.java
@@ -2,4 +2,27 @@ package br.com.etec.model;
 
 public class Operacoes {
 	
+	@FXML
+	private TextFileld txfUsuario;
+	
+	@FXML
+	private PasswordField psfSenha;
+	
+	@FXML
+	private Button btnAcessar;
+	
+	@FXML
+	private Button btnCadastrar;
+	
+	
+	
+	
+	@FXML
+	private void validarUsuario(ActionEvent event) {
+		String nomeUsuario,
+		nomeUsuario = txfUsuario.getText(),
+		String senhaUsuario,
+		senhaUsuario = txfUsuario getText(),
+	}
+	
 }

76dbe6dd3a93c65d5478337e57e6f648d85dd62a
lab04 <lab04@lab4-10>
2024-09-02 20:48:41 -0300

    atualizacao da aula que o professor fez

--- LojaEtec/src/br/com/etec/model/Operacoes.java
+++ LojaEtec/src/br/com/etec/model/Operacoes.java
@@ -1,28 +1,47 @@
 package br.com.etec.model;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Operacoes {
 	
 	@FXML
-	private TextFileld txfUsuario;
-	
+	private TextField txfUsuario;
 	@FXML
 	private PasswordField psfSenha;
-	
 	@FXML
 	private Button btnAcessar;
-	
 	@FXML
 	private Button btnCadastrar;
 	
-	
-	
-	
 	@FXML
 	private void validarUsuario(ActionEvent event) {
-		String nomeUsuario,
-		nomeUsuario = txfUsuario.getText(),
-		String senhaUsuario,
-		senhaUsuario = txfUsuario getText(),
+		String nomeUsuario;
+		nomeUsuario = txfUsuario.getText();
+		String senhaUsuario;
+		senhaUsuario = psfSenha.getText();
+		
+		if(nomeUsuario.isEmpty() || senhaUsuario.isEmpty()) {
+			if(nomeUsuario.isEmpty()) {
+				mostrarAlerta(Alert.AlertType.WARNING, "INFORME O NOME", "É necessario colocar o nome do usuario.");
+			} else{
+				if(senhaUsuario.isEmpty()) {
+					mostrarAlerta(Alert.AlertType.WARNING, "INFORME O NOME", "É necessario colocar o nome do usuario.");
+				}
+			}
+		}
 	}
-	
+	private void mostrarAlerta(Alert.AlertType tipo, String titulo, String mensagem) {
+		Alert alerta = new Alert(tipo);
+		alerta.setTitle(titulo);
+		alerta.setContentText(mensagem);
+		alerta.showAndWait();
+	}
 }
+
+
+
